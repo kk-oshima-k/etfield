16ab4a0c3277360a2d98622370511ca87b7805e5
m21078 <m21078@oshima-k.ac.jp>
2025-09-01 11:00:44 +0900

    ボトルを見つけるシーンの追加 getHSVを一本化

--- ETRobo/Scenario1/Scenario1.h
+++ ETRobo/Scenario1/Scenario1.h
@@ -5,4 +5,65 @@
 #include "../../Scene/LineTraceScene.h"
 #include "../../Scene/StraightScene.h"
 #include "../../Scene/RotationScene.h"
-#include
+#include "../../Scene/FindBottleWithLineTraceScene.h"
+
+class Scenario1Scene1 : public FindBottleWithLineTraceSceneace {
+public:
+  Scenario1Scene1(DriveController &driveController, const ColorSensorController &colorSensorController, const UltrasonicSensorController &ultrasonicSensorController,
+          int velocity = 300, bool rightEdge = false, const PIDParameters &pidParameters = {0.25, 0.005*0, 0.25*0}, int target = 525,
+          LineColor color = LineColor::LINE_COLOR_NONE,
+          int thresholdDistance = 100,
+          int bottleDistace = 10) :
+    FindBottleWithLineTraceScene(driveController, colorSensorController, velocity, rightEdge, pidParameters, target, color, thresholdDistance, bottleDistace) {}
+};
+class Scenario1Scene2 : public RotationScene {
+public:
+  Scenario1Scene2(DriveController &driveController, int angularVelocity = 100, int thresholdAngle = 100) :
+    RotationScene(driveController, angularVelocity, thresholdAngle) {}
+};
+class Scenario1Scene3 : public StraightScene {
+public:
+  Scenario1Scene3(DriveController &driveController, const ColorSensorController &colorSensorController,
+          int velocity = 100,
+          LineColor color = LineColor::LINE_COLOR_NONE,
+          int thresholdDistance = 100) :
+    StraightScene(driveController, colorSensorController, velocity, color, thresholdDistance) {}
+};
+class Scenario1Scene4 : public RotationScene {
+public:
+  Scenario1Scene4(DriveController &driveController, int angularVelocity = 100, int thresholdAngle = 100) :
+    RotationScene(driveController, angularVelocity, thresholdAngle) {}
+};
+class Scenario1Scene5 : public StraightScene {
+public:
+  Scenario1Scene5(DriveController &driveController, const ColorSensorController &colorSensorController,
+          int velocity = 100,
+          LineColor color = LineColor::LINE_COLOR_BLACK,
+          int thresholdDistance = 0) :
+    StraightScene(driveController, colorSensorController, velocity, color, thresholdDistance) {}
+};
+class Scenario1Scene6 : public RotationScene {
+public:
+  Scenario1Scene6(DriveController &driveController, int angularVelocity = 100, int thresholdAngle = 100) :
+    RotationScene(driveController, angularVelocity, thresholdAngle) {}
+};
+class Scenario1Scene7 : public LineTraceScene {
+public:
+  Scenario1Scene7(DriveController &driveController, const ColorSensorController &colorSensorController,
+          int velocity = 100, bool rightEdge = false, const PIDParameters &pidParameters = {1.0, 0.1, 0.01}, int target = 127,
+          LineColor color = LineColor::LINE_COLOR_BLUE,
+          int thresholdDistance = 100) :
+    LineTraceScene(driveController, colorSensorController, velocity, rightEdge, pidParameters, target, color, thresholdDistance) {}
+};
+
+class Scenario1 : public Scenario {
+public:
+  Scenario1(DriveController &driveController, const ColorSensorController &colorSensorController, const UltrasonicSensorController &ultrasonicSensorController);
+
+  int process_scene() override;
+
+  int switch_scene(int scene_index);
+
+};
+
+#endif // SCENARIO1_H
